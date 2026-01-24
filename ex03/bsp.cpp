3b99520f06f9f488d8e99cef983ea52a181bdf00
Davi Santos <davisimoesdossantos1@gmail.com>
2026-01-24 16:20:50 -0300

    feat: implemented BSP successfully :)

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,18 +6,21 @@
 /*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 23:13:20 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/01/24 12:13:14 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/01/24 16:19:00 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-	Reduced line equation is y = mx + n, where m is the angular coefficient.
+	The idea is to calculate the general line equation for each line, AB, BC and CA,
+	then I'd place the other vertice into such equation and the point and compare their
+	sign, If the 3 comparisons hold true, then the point is inside the triangle,
+	otherwise the point is not inside the triangle.
+	
+	Ax + By + c = 0
 
-	To calculate m you need 2 points and you do:
-							m = (y2 - y1) / (x2 - x1)
-
-	To calculate n you need m and one set of coordinates, so you do:
-							n = y1 - mx1
+	A = (y1 - y2)
+	B = (x2 - x1)
+	C = (x1y2 - x2y1)
 */
 
 #include "Fixed.hpp"
@@ -25,26 +28,29 @@
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	// Angular coefficient
-	Fixed	mAB = Fixed((b.getY() - a.getY()) / (b.getX() - a.getX()));
-	Fixed	mBC = Fixed((c.getY() - b.getY()) / (c.getX() - b.getX()));
-	Fixed	mCA = Fixed((a.getY() - c.getY()) / (a.getX() - c.getX()));
-
-	// Linear coefficient
-	Fixed	nAB = Fixed(a.getY() - (mAB.toFloat() * a.getX()));
-	Fixed	nBC = Fixed(b.getY() - (mBC.toFloat() * b.getX()));
-	Fixed	nCA = Fixed(c.getY() - (mCA.toFloat() * c.getX()));
+	// line AB
+	Fixed aAB = Fixed( a.getY() - b.getY() );
+	Fixed bAB = Fixed( b.getX() - a.getX() );
+	Fixed cAB = Fixed( (a.getX() * b.getY()) - (b.getX() * a.getY()) );
 	
-	// Point comparison on the line
-	Fixed	pAB = Fixed((mAB.toFloat() * point.getX()) + nAB.toFloat() - point.getY());
-	Fixed	pBC = Fixed((mBC.toFloat() * point.getX()) + nBC.toFloat() - point.getY());
-	Fixed	pCA = Fixed((mCA.toFloat() * point.getX()) + nCA.toFloat() - point.getY());
+	// line BC
+	Fixed aBC = Fixed( b.getY() - c.getY() );
+	Fixed bBC = Fixed( c.getX() - b.getX() );
+	Fixed cBC = Fixed( (b.getX() * c.getY()) - (c.getX() * b.getY()) );
 
-	if (!pAB.toInt() || !pBC.toInt() || !pCA.toInt())
-		return (false);
-	if (pAB.toFloat() > 0 && pBC.toFloat() > 0 && pCA.toInt() > 0)
+	// line CA
+	Fixed aCA = Fixed( c.getY() - a.getY() );
+	Fixed bCA = Fixed( a.getX() - c.getX() );
+	Fixed cCA = Fixed( (c.getX() * a.getY()) - (a.getX() * c.getY()) );
+	
+	// point locations
+	Fixed pAB = Fixed( (point.getX() * aAB.toFloat()) + (point.getY() * bAB.toFloat()) + cAB.toFloat() );
+	Fixed pBC = Fixed( (point.getX() * aBC.toFloat()) + (point.getY() * bBC.toFloat()) + cBC.toFloat() );
+	Fixed pCA = Fixed( (point.getX() * aCA.toFloat()) + (point.getY() * bCA.toFloat()) + cCA.toFloat() );
+
+	if (pAB.toFloat() > 0 && pBC.toFloat() > 0 && pCA.toFloat() > 0)
 		return (true);
-	if (pAB.toFloat() < 0 && pBC.toFloat() < 0 && pCA.toInt() < 0)
+	if (pAB.toFloat() < 0 && pBC.toFloat() < 0 && pCA.toFloat() < 0)
 		return (true);
 	return (false);
 }
